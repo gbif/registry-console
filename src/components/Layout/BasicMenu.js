c60fe28149cfccf5e0bd0cc8074ca2a2ebc25881
Alexandr Ketov <ketov.alex.v@gmail.com>
2018-12-15 10:41:24 +0600

    Refactored BasicMenu: added config and generators + integrated permissions check.

--- src/components/Layout/BasicMenu.js
+++ src/components/Layout/BasicMenu.js
@@ -2,11 +2,60 @@ import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import { FormattedMessage } from 'react-intl';
 import { Menu, Icon } from 'antd';
+import { connect } from 'react-redux';
+
 import Logo from './Logo';
+import MenuConfig from '../../config/menu.config';
 
 const SubMenu = Menu.SubMenu;
 
 class BasicMenu extends Component {
+  renderMenu = () => {
+    return MenuConfig.map(el => {
+      if (el.type === 'submenu') {
+        return this.renderSubmenu(el);
+      }
+
+      return this.renderItem(el);
+    });
+  };
+
+  renderSubmenu = menu => {
+    if (!this.isAuthorised(menu.authority)) {
+      return null;
+    }
+
+    return (
+      <SubMenu key={menu.key} title={
+        <div>
+          {menu.title.icon && <Icon type={menu.title.icon}/>}
+          <FormattedMessage id={menu.title.message.id} defaultMessage={menu.title.message.default}/>
+        </div>
+      }>
+        {menu.children.map(this.renderItem)}
+      </SubMenu>
+    );
+  };
+
+  renderItem = item => {
+    if (!this.isAuthorised(item.authority)) {
+      return null;
+    }
+
+    return (
+      <Menu.Item key={item.key}>
+        {item.title.icon && <Icon type={item.title.icon}/>}
+        <FormattedMessage id={item.title.message.id} defaultMessage={item.title.message.default}/>
+      </Menu.Item>
+    );
+  };
+
+  isAuthorised = authority => {
+    const { user } = this.props;
+
+    return !authority || (user && user.roles.some(role => authority.includes(role)));
+  };
+
   render() {
     const { location, history } = this.props;
     return (
@@ -16,127 +65,24 @@ class BasicMenu extends Component {
             <Logo/>
             <h1>
               <FormattedMessage id="orgName" defaultMessage="GBIF Registry"/>
-              </h1>
+            </h1>
           </a>
         </div>
         <Menu
-          onClick={(e) => {
-            history.push(e.key);
-          }}
+          onClick={(e) => history.push(e.key)}
           defaultSelectedKeys={[location.pathname]}
           defaultOpenKeys={[location.pathname.split('/')[1]]}
           mode="inline"
           theme="dark"
           inlineCollapsed={this.props.collapsed}
         >
-          <SubMenu key="organization" title={
-            <div>
-              <Icon type="share-alt"/>
-              <FormattedMessage id="menu.organization" defaultMessage="Organizations"/>
-            </div>
-          }>
-            <Menu.Item key="/organization/search">
-              <FormattedMessage id="menu.search" defaultMessage="Search"/>
-            </Menu.Item>
-            <Menu.Item key="/organization/deleted">
-              <FormattedMessage id="menu.deleted" defaultMessage="Deleted"/>
-            </Menu.Item>
-            <Menu.Item key="/organization/pending">
-              <FormattedMessage id="menu.pending" defaultMessage="Pending"/>
-            </Menu.Item>
-            <Menu.Item key="/organization/nonPublishing">
-              <FormattedMessage id="menu.organizationNonPublishing" defaultMessage="Non publishing organizations"/>
-            </Menu.Item>
-            <Menu.Item key="/organization/create">
-              <FormattedMessage id="menu.createOrganization" defaultMessage="Create new organization"/>
-            </Menu.Item>
-          </SubMenu>
-
-          <SubMenu key="dataset" title={
-            <div>
-              <Icon type="table"/>
-              <FormattedMessage id="menu.dataset" defaultMessage="Datasets"/>
-            </div>
-          }>
-            <Menu.Item key="/dataset/search">
-              <FormattedMessage id="menu.search" defaultMessage="Search"/>
-            </Menu.Item>
-            <Menu.Item key="/dataset/deleted">
-              <FormattedMessage id="menu.deleted" defaultMessage="Deleted"/>
-            </Menu.Item>
-            <Menu.Item key="/dataset/duplicate">
-              <FormattedMessage id="menu.duplicate" defaultMessage="Duplicate"/>
-            </Menu.Item>
-            <Menu.Item key="/dataset/constituent">
-              <FormattedMessage id="menu.constituent" defaultMessage="Constituent"/>
-            </Menu.Item>
-            <Menu.Item key="/dataset/withNoEndpoint">
-              <FormattedMessage id="menu.withNoEndpoint" defaultMessage="With no endpoint"/>
-            </Menu.Item>
-            <Menu.Item key="/dataset/create">
-              <FormattedMessage id="menu.createDataset" defaultMessage="Create new dataset"/>
-            </Menu.Item>
-          </SubMenu>
-
-          <SubMenu key="installation" title={
-            <div>
-              <Icon type="hdd"/>
-              <FormattedMessage id="menu.installation" defaultMessage="Installations"/>
-            </div>
-          }>
-            <Menu.Item key="/installation/search">
-              <FormattedMessage id="menu.search" defaultMessage="Search"/>
-            </Menu.Item>
-            <Menu.Item key="/installation/deleted">
-              <FormattedMessage id="menu.deleted" defaultMessage="Deleted"/>
-            </Menu.Item>
-            <Menu.Item key="/installation/nonPublishing">
-              <FormattedMessage id="menu.installationNoDataset" defaultMessage="Serving no datasets"/>
-            </Menu.Item>
-            <Menu.Item key="/installation/create">
-              <FormattedMessage id="menu.createInstallation" defaultMessage="Create new installation"/>
-            </Menu.Item>
-          </SubMenu>
-
-          <SubMenu key="grbio" title={
-            <div>
-              <Icon type="api"/>
-              <FormattedMessage id="menu.grbio" defaultMessage="GRBIO"/>
-            </div>
-          }>
-            <Menu.Item key="/grbio/collection/search">
-              <FormattedMessage id="menu.collection" defaultMessage="Collections"/>
-            </Menu.Item>
-            <Menu.Item key="/grbio/institution/search">
-              <FormattedMessage id="menu.institution" defaultMessage="Institutions"/>
-            </Menu.Item>
-            <Menu.Item key="/grbio/person/search">
-              <FormattedMessage id="menu.person" defaultMessage="Persons"/>
-            </Menu.Item>
-          </SubMenu>
-
-          <SubMenu key="node" title={
-            <div>
-              <Icon type="mail"/>
-              <FormattedMessage id="menu.node" defaultMessage="Nodes"/>
-            </div>
-          }>
-            <Menu.Item key="/node/search">
-              <FormattedMessage id="menu.search" defaultMessage="Search"/>
-            </Menu.Item>
-            <Menu.Item key="/node/create">
-              <FormattedMessage id="menu.createNode" defaultMessage="Create new node"/>
-            </Menu.Item>
-          </SubMenu>
-
-          <Menu.Item key="/user/search">
-            <Icon type="user"/><FormattedMessage id="menu.user" defaultMessage="Users"/>
-          </Menu.Item>
-
+          {this.renderMenu()}
         </Menu>
       </React.Fragment>
     );
   }
 }
 
-export default withRouter(BasicMenu);
+const mapStateToProps = ({ user }) => ({ user });
+
+export default connect(mapStateToProps)(withRouter(BasicMenu));
---
+++ src/config/menu.config.js
@@ -0,0 +1,283 @@
+export default [
+  {
+    type: 'submenu',
+    key: 'organization',
+    title: {
+      icon: 'share-alt',
+      message: {
+        id: 'menu.organization',
+        default: 'Organizations'
+      }
+    },
+    children: [
+      {
+        type: 'item',
+        key: '/organization/search',
+        title: {
+          message: {
+            id: 'menu.search',
+            default: 'Search'
+          }
+        }
+      },
+      {
+        type: 'item',
+        key: '/organization/deleted',
+        title: {
+          message: {
+            id: 'menu.deleted',
+            default: 'Deleted'
+          }
+        }
+      },
+      {
+        type: 'item',
+        key: '/organization/pending',
+        title: {
+          message: {
+            id: 'menu.pending',
+            default: 'Pending'
+          }
+        }
+      },
+      {
+        type: 'item',
+        key: '/organization/nonPublishing',
+        title: {
+          message: {
+            id: 'menu.organizationNonPublishing',
+            default: 'Non publishing organizations'
+          }
+        }
+      },
+      {
+        type: 'item',
+        key: '/organization/create',
+        title: {
+          message: {
+            id: 'menu.createOrganization',
+            default: 'Create new organization'
+          }
+        },
+        authority: ['USER']
+      }
+    ]
+  },
+  {
+    type: 'submenu',
+    key: 'dataset',
+    title: {
+      icon: 'table',
+      message: {
+        id: 'menu.dataset',
+        default: 'Datasets'
+      }
+    },
+    children: [
+      {
+        type: 'item',
+        key: '/dataset/search',
+        title: {
+          message: {
+            id: 'menu.search',
+            default: 'Search'
+          }
+        }
+      },
+      {
+        type: 'item',
+        key: '/dataset/deleted',
+        title: {
+          message: {
+            id: 'menu.deleted',
+            default: 'Deleted'
+          }
+        }
+      },
+      {
+        type: 'item',
+        key: '/dataset/duplicate',
+        title: {
+          message: {
+            id: 'menu.duplicate',
+            default: 'Duplicate'
+          }
+        }
+      },
+      {
+        type: 'item',
+        key: '/dataset/constituent',
+        title: {
+          message: {
+            id: 'menu.constituent',
+            default: 'Constituent'
+          }
+        }
+      },
+      {
+        type: 'item',
+        key: '/dataset/withNoEndpoint',
+        title: {
+          message: {
+            id: 'menu.withNoEndpoint',
+            default: 'With no endpoint'
+          }
+        }
+      },
+      {
+        type: 'item',
+        key: '/dataset/create',
+        title: {
+          message: {
+            id: 'menu.createDataset',
+            default: 'Create new dataset'
+          }
+        },
+        authority: ['USER']
+      }
+    ]
+  },
+  {
+    type: 'submenu',
+    key: 'installation',
+    title: {
+      icon: 'hdd',
+      message: {
+        id: 'menu.installation',
+        default: 'Installations'
+      }
+    },
+    children: [
+      {
+        type: 'item',
+        key: '/installation/search',
+        title: {
+          message: {
+            id: 'menu.search',
+            default: 'Search'
+          }
+        }
+      },
+      {
+        type: 'item',
+        key: '/installation/deleted',
+        title: {
+          message: {
+            id: 'menu.deleted',
+            default: 'Deleted'
+          }
+        }
+      },
+      {
+        type: 'item',
+        key: '/installation/nonPublishing',
+        title: {
+          message: {
+            id: 'menu.installationNoDataset',
+            default: 'Serving no datasets'
+          }
+        }
+      },
+      {
+        type: 'item',
+        key: '/installation/create',
+        title: {
+          message: {
+            id: 'menu.createInstallation',
+            default: 'Create new installation'
+          }
+        },
+        authority: ['USER']
+      }
+    ]
+  },
+  {
+    type: 'submenu',
+    key: 'grbio',
+    title: {
+      icon: 'api',
+      message: {
+        id: 'menu.grbio',
+        default: 'GRBIO'
+      }
+    },
+    children: [
+      {
+        type: 'item',
+        key: '/grbio/collection/search',
+        title: {
+          message: {
+            id: 'menu.collection',
+            default: 'Collections'
+          }
+        }
+      },
+      {
+        type: 'item',
+        key: '/grbio/institution/search',
+        title: {
+          message: {
+            id: 'menu.institution',
+            default: 'Institutions'
+          }
+        }
+      },
+      {
+        type: 'item',
+        key: '/grbio/person/search',
+        title: {
+          message: {
+            id: 'menu.person',
+            default: 'Persons'
+          }
+        }
+      }
+    ]
+  },
+  {
+    type: 'submenu',
+    key: 'node',
+    title: {
+      icon: 'mail',
+      message: {
+        id: 'menu.node',
+        default: 'Nodes'
+      }
+    },
+    children: [
+      {
+        type: 'item',
+        key: '/node/search',
+        title: {
+          message: {
+            id: 'menu.search',
+            default: 'Search'
+          }
+        }
+      },
+      {
+        type: 'item',
+        key: '/node/create',
+        title: {
+          message: {
+            id: 'menu.createNode',
+            default: 'Create new node'
+          }
+        },
+        authority: ['USER']
+      }
+    ]
+  },
+  {
+    type: 'item',
+    key: '/user/search',
+    title: {
+      icon: 'user',
+      message: {
+        id: 'menu.user',
+        default: 'Users'
+      }
+    },
+    authority: ['REGISTRY_ADMIN']
+  }
+];
