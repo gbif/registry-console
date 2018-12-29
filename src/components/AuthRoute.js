59e04c1013ed06ea9b8471e54afc9d75c91a79e3
Alexandr Ketov <ketov.alex.v@gmail.com>
2018-12-29 09:35:33 +0600

    Fix bug with AuthRouter re-render on child update.

--- src/components/AuthRoute.js
+++ src/components/AuthRoute.js
@@ -6,9 +6,17 @@ import Exception403 from './exception/403';
 import withContext from './hoc/withContext';
 import { canCreateItem } from './helpers';
 
+/**
+ * A custom Route wrapper for the routes that have permission restrictions
+ */
+class AuthRoute extends React.Component {
+  shouldComponentUpdate(nextProps, nextState, nextContext) {
+    const { user, location } = this.props;
+    // If the url and the user are the same we shouldn't re-render component
+    return nextProps.user !== user || nextProps.location.pathname !== location.pathname;
+  }
 
-const AuthRoute = ({ user, roles, type, component: Comp, ...rest }) => {
-  const isAuthorized = () => {
+  isAuthorized(user, roles, type) {
     if (!roles || (user && user.roles.includes('REGISTRY_ADMIN'))) {
       return true;
     }
@@ -20,18 +28,23 @@ const AuthRoute = ({ user, roles, type, component: Comp, ...rest }) => {
     return false;
   };
 
-  return <Route {...rest} render={props => {
-    if (isAuthorized()) {
-      return <Comp {...props}/>;
-    } else {
-      return <Exception403/>;
-    }
-  }}/>;
-};
+  render() {
+    console.log('rendering...');
+    const { user, roles, type, component: Comp, ...rest } = this.props;
+
+    return <Route {...rest} render={props => {
+      if (this.isAuthorized(user, roles, type)) {
+        return <Comp {...props}/>;
+      } else {
+        return <Exception403/>;
+      }
+    }}/>;
+  }
+}
 
 AuthRoute.propTypes = {
-  roles: PropTypes.array.isRequired,
-  type: PropTypes.string
+  roles: PropTypes.array.isRequired, // list or roles for which wrapped content should be available
+  type: PropTypes.string // type of item for the case or create Route
 };
 
 const mapContextToProps = ({ user }) => ({ user });
