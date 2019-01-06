b3623f82daa0e6a37ce46d0d5ad2107e9351a62a
Alexandr Ketov <ketov.alex.v@gmail.com>
2019-01-06 19:26:11 +0600

    #127. Fix bug in PermissionWrapper. Add tests for PageWrapper and PermissionWrapper.

--- src/__mocks__/context.mock.js
+++ src/__mocks__/context.mock.js
@@ -8,9 +8,27 @@ export const mockedContext = {
   addError: jest.fn()
 };
 
+export const user = {
+  userName: 'test',
+  roles: ['USER']
+};
+
 export const userEditor = {
   userName: 'editor',
-  roles: ['USER', 'REGISTRY_EDITOR']
+  roles: ['USER', 'REGISTRY_EDITOR'],
+  editorRoleScopes: ['0c1f1074-48e8-46d3-abad-9ee264ab8c8e']
+};
+
+export const userEditorWithNode = {
+  userName: 'editor',
+  roles: ['USER', 'REGISTRY_EDITOR'],
+  editorRoleScopeItems: [{ type: 'node' }]
+};
+
+export const userEditorWithOrganization = {
+  userName: 'editor',
+  roles: ['USER', 'REGISTRY_EDITOR'],
+  editorRoleScopeItems: [{ type: 'organization' }]
 };
 
 export const userAdmin = {
