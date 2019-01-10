0c380331217bc5b2e9052963b55364280d71bda5
Alexandr Ketov <ketov.alex.v@gmail.com>
2019-01-10 09:50:13 +0600

    #147. Added number format.

--- src/components/common/ItemMenu.js
+++ src/components/common/ItemMenu.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NavLink, withRouter } from 'react-router-dom';
 import { Col, Menu, Row } from 'antd';
-import { FormattedMessage } from 'react-intl';
+import { FormattedMessage, FormattedNumber } from 'react-intl';
 import PropTypes from 'prop-types';
 import injectSheet from 'react-jss';
 
@@ -23,6 +23,17 @@ const styles = {
   },
   menu: {
     height: '100%'
+  },
+  link: {
+    '& span + span': {
+      margin: '0 5px',
+      '&:before': {
+        content: '"("'
+      },
+      '&:after': {
+        content: '")"'
+      }
+    }
   }
 };
 
@@ -91,9 +102,9 @@ const ItemMenu = props => {
           return isAuthorised(item.roles) && (!isNew || !item.hideOnNew);
         }).map(item => (
           <Menu.Item key={item.key}>
-            <NavLink to={getURL(item)}>
+            <NavLink to={getURL(item)} className={classes.link}>
               <FormattedMessage id={item.title.id} defaultMessage={item.title.default}/>
-              {item.subtype ? ` (${counts[item.count] || 0})` : null}
+              {item.subtype ? <FormattedNumber value={counts[item.count] || 0} /> : null}
             </NavLink>
           </Menu.Item>
         ))}
@@ -119,10 +130,10 @@ const ItemMenu = props => {
   return (
     <div className={classes.container}>
       <Row type="flex" justify="start">
-        <Col style={{ width: width <= SMALL ? '100%' : '200px' }}>
+        <Col style={{ width: width <= SMALL ? '100%' : '210px' }}>
           {renderMenu()}
         </Col>
-        <Col className={classes.content} style={{ width: width <= SMALL ? '100%' : 'calc(100% - 200px)' }}>
+        <Col className={classes.content} style={{ width: width <= SMALL ? '100%' : 'calc(100% - 210px)' }}>
           {children}
         </Col>
       </Row>
