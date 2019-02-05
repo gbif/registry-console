a860f88c60f9437ce9e9b021eef247d267c7723a
luckyluck <ketov.alex.v@gmail.com>
2019-02-05 17:48:05 +0600

    #205. Renewing auth token after every response if user logged in.

--- src/api/util/axiosInstance.js
+++ src/api/util/axiosInstance.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
+
 import config from './config';
+import {JWT_STORAGE_NAME} from "../../components/auth/user";
 
 // Creating axios custom instance with global base URL
 const instance = axios.create({
@@ -8,7 +10,19 @@ const instance = axios.create({
 
 // Add a request interceptor
 instance.interceptors.response.use(
-  response => response,
+  response => {
+    const {token} = response.headers;
+    if (token) {
+      // Renewing user's token
+      sessionStorage.setItem(JWT_STORAGE_NAME, token);
+      instance.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+      // Also, we should update token in localStorage if user chose to be logged in
+      if (localStorage.getItem(JWT_STORAGE_NAME) !== null) {
+        localStorage.setItem(JWT_STORAGE_NAME, token);
+      }
+    }
+    return response;
+  },
   error => {
     // If a Network error
     if (!error.response) {
