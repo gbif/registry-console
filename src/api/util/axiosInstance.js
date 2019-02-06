d34ad21b5f89cb59edcb3ffb755ef19632901a72
Morten Høfft <mhoefft@gbif.org>
2019-02-06 13:58:41 +0100

    relates to https://github.com/gbif/registry-console/issues/205

--- src/api/util/axiosInstance.js
+++ src/api/util/axiosInstance.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import config from './config';
+export const JWT_STORAGE_NAME = 'jwt';
 
 // Creating axios custom instance with global base URL
 const instance = axios.create({
@@ -8,7 +9,19 @@ const instance = axios.create({
 
 // Add a request interceptor
 instance.interceptors.response.use(
-  response => response,
+  response => {
+    const { token } = response.headers;
+
+    if (token && sessionStorage.getItem(JWT_STORAGE_NAME) !== null) {
+      instance.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+      sessionStorage.setItem(JWT_STORAGE_NAME, token);
+      if (localStorage.getItem(JWT_STORAGE_NAME)) {
+        localStorage.setItem(JWT_STORAGE_NAME, token);
+      }
+    }
+    return response;
+  },
+  // response => response,
   error => {
     // If a Network error
     if (!error.response) {
