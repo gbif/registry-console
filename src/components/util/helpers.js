53712a6a08d5a8ee408b8d597e6b7b208ee9c9e3
Alexandr Ketov <ketov.alex.v@gmail.com>
2019-02-21 19:13:31 +0600

    #89. Fixed bug clearing lat/lng fields; set maximum map bounds to avoid selecting invalid lat/lng values.

--- src/components/util/helpers.js
+++ src/components/util/helpers.js
@@ -80,3 +80,13 @@ export const simplifyHttpUrls = url => {
  * @type {string}
  */
 export const defaultNameSpace = 'default-term.gbif.org';
+
+export const isValidLatitude = value => {
+  const latitude = parseFloat(value);
+  return Number.isFinite(latitude) && latitude <= 90 && latitude >= -90;
+};
+
+export const isValidLongitude = value => {
+  const longitude = parseFloat(value);
+  return Number.isFinite(longitude) && longitude <= 180 && longitude >= -180;
+};
