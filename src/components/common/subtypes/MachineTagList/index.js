b139317191738cb9dbccb1319dc7734662c96ada
Morten Høfft <mhoefft@gbif.org>
2019-05-09 10:28:51 +0200

    closes https://github.com/gbif/registry-console/issues/286

--- src/components/common/subtypes/MachineTagList/index.js
+++ src/components/common/subtypes/MachineTagList/index.js
@@ -4,7 +4,7 @@ import { List, Button, Row, Col, Icon, Tooltip } from 'antd';
 import { FormattedMessage, injectIntl, FormattedNumber } from 'react-intl';
 
 // Wrappers
-import { HasScope } from '../../../auth';
+import { HasScope, HasRole, roles } from '../../../auth';
 import withWidth, { MEDIUM } from '../../../hoc/Width';
 import withContext from '../../../hoc/withContext';
 // Components
@@ -108,11 +108,11 @@ class MachineTagList extends React.Component {
             </Col>
 
             <Col xs={12} sm={12} md={8} className="text-right">
-              <HasScope uuids={uuids}>
+              <HasRole roles={roles.REGISTRY_ADMIN}>
                 <Button htmlType="button" type="primary" onClick={() => this.showModal()}>
                   <FormattedMessage id="createNew" defaultMessage="Create new"/>
                 </Button>
-              </HasScope>
+              </HasRole>
             </Col>
           </Row>
 
@@ -130,14 +130,14 @@ class MachineTagList extends React.Component {
             renderItem={item => (
               <List.Item
                 actions={[
-                  <HasScope uuids={uuids}>
+                  <HasRole roles={roles.REGISTRY_ADMIN}>
                     <ConfirmButton
                       title={confirmTitle}
                       btnText={<FormattedMessage id="delete" defaultMessage="Delete"/>}
                       onConfirm={() => this.deleteMachineTag(item)}
                       type={'link'}
                     />
-                  </HasScope>
+                  </HasRole>
                 ]}
                 style={width < MEDIUM ? { flexDirection: 'column' } : {}}
               >
