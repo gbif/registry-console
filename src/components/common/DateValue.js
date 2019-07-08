c591dfbf08674c4dd9f9de0db43beed295cc3bcd
Morten Høfft <mhoefft@gbif.org>
2019-07-08 16:27:43 +0200

    closes https://github.com/gbif/registry-console/issues/299

--- src/components/common/DateValue.js
+++ src/components/common/DateValue.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import moment from 'moment';
 import injectSheet from 'react-jss';
+import { Tooltip } from 'antd';
 
 const styles = {
   date: {
@@ -16,8 +17,10 @@ const DateValue = ({ value, classes }) => {
 
   return (
     <div className={classes.date}>
-      <span>{moment.utc(value).format('ddd DD MMM YYYY HH:mm:ss')}</span>
-      <span>(UTC {moment(value).format('Z')})</span>
+      <Tooltip placement="topLeft" title={`${moment(value).format('ddd DD MMM YYYY HH:mm:ss')} (UTC ${moment(value).format('Z')}`}>
+        <span>{moment.utc(value).format('ddd DD MMM YYYY HH:mm:ss')}</span>
+        {/* <span>(UTC {moment.utc(value).format('Z')})</span> */}
+      </Tooltip>
     </div>
   );
 };
