20a42f5e73ab1595deed37dddf5afb2bf9d58757
Morten Høfft <mhoefft@gbif.org>
2019-11-08 10:02:14 +0100

    reformat

--- src/api/util/util.js
+++ src/api/util/util.js
@@ -1,8 +1,8 @@
-export const eachLimit = async (collection, limit,iteratorFunction) => {
-    let index = 0;
-    while (index < collection.length){
-      await Promise.all(collection.slice(index, index+limit).map(iteratorFunction))
-      index += limit;
-    } 
-      return collection
-    }
+export const eachLimit = async (collection, limit, iteratorFunction) => {
+  let index = 0;
+  while (index < collection.length) {
+    await Promise.all(collection.slice(index, index + limit).map(iteratorFunction))
+    index += limit;
+  }
+  return collection
+}
