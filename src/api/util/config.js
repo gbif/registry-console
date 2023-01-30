1bbfb626d70e69a19024919004054845d0843517
Matthew Blissett <mblissett@gbif.org>
2023-01-30 19:57:44 +0100

    Dev2 environment.

--- src/api/util/config.js
+++ src/api/util/config.js
@@ -1,7 +1,8 @@
 const logIndices = {
   prod: '24993300-fdb1-11ea-93de-b97c40066ce8',
   uat: '782daf00-fdb1-11ea-93de-b97c40066ce8',
-  dev: '36e5ccd0-fdb1-11ea-93de-b97c40066ce8'
+  dev: '36e5ccd0-fdb1-11ea-93de-b97c40066ce8',
+  dev: '00000000-0000-0000-0000-000000000000'
 };
 const datasetLatestLogs  = "https://logs.gbif.org/app/discover#/?_g=(filters:!(),refreshInterval:(display:On,pause:!f,value:0),time:(from:now-7d,to:now))&_a=(columns:!(_source),filters:!(),index:'{{INDEX}}',interval:auto,query:(language:lucene,query:'datasetKey.keyword:%22{{UUID}}%22'),sort:!('@timestamp',desc))";
 const datasetAttemptLogs = "https://logs.gbif.org/app/discover#/?_g=(filters:!(),refreshInterval:(display:On,pause:!f,value:0),time:(from:now-1y,to:now))&_a=(columns:!(_source),filters:!(),index:'{{INDEX}}',interval:auto,query:(language:lucene,query:'datasetKey.keyword:%22{{UUID}}%22%20AND%20attempt:%22{{ATTEMPT}}%22'),sort:!('@timestamp',desc))";
@@ -55,6 +56,25 @@ const environments = {
       datasetAttempt: process.env.REACT_APP_LOG_DATASET_ATTEMPT || datasetAttemptLogs.replace('{{INDEX}}', logIndices.dev)
     }
   },
+  dev2: {
+    gbifUrl: 'https://www.gbif-dev2.org',
+    dataApi: 'https://registry-api.gbif-dev2.org',
+    dataApi_v1: 'https://api.gbif-dev2.org/v1',
+    secretariatNode: '02c40d2a-1cba-4633-90b7-e36e5e97aba8',
+    env: 'dev2',
+    languages: [
+      { key: 'en', code: '🇬🇧', name: 'English' },
+      { key: 'he', code: 'HE', name: 'Hebrew' },
+      { key: 'kk', code: '🇰🇿', name: 'Қазақша' },
+      { key: 'da', code: '🇩🇰', name: 'Dansk' },
+      { key: 'es', name: 'Español' },
+      { key: 'fr', name: 'Français' },
+    ],
+    logLinks: {
+      datasetLatest: process.env.REACT_APP_LOG_DATASET_LATEST || datasetLatestLogs.replace('{{INDEX}}', logIndices.dev2),
+      datasetAttempt: process.env.REACT_APP_LOG_DATASET_ATTEMPT || datasetAttemptLogs.replace('{{INDEX}}', logIndices.dev2)
+    }
+  },
   local: {
     gbifUrl: process.env.REACT_APP_URL || 'https://www.gbif-uat.org',
     dataApi: process.env.REACT_APP_API || 'https://registry-api.gbif-uat.org',
@@ -81,10 +101,10 @@ if (domain.endsWith('gbif.org')) {
   env = environments.prod;
 } else if (domain.endsWith('gbif-uat.org')) {
   env = environments.uat;
-} else if (domain.endsWith('demo.gbif-dev.org')) {
-  env = process.env.REACT_APP_URL || environments.demo;
 } else if (domain.endsWith('gbif-dev.org')) {
   env = process.env.REACT_APP_URL || environments.dev;
+} else if (domain.endsWith('gbif-dev2.org')) {
+  env = process.env.REACT_APP_URL || environments.dev2;
 }
 
 export default env;
